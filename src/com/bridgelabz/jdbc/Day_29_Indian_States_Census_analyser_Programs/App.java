96f48e2a8071f2f5cc207d73981edfb7667ef53a
“thaheer” <thaheer43@gmail.com>
2022-07-20 22:06:47 +0530

    Added : Logic for Census Analyser

---
+++ src/com/bridgelabz/jdbc/Day_29_Indian_States_Census_analyser_Programs/App.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.jdbc.Day_29_Indian_States_Census_analyser_Programs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
